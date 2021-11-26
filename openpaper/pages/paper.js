9382d86ce143025cc940ef40278f69b519d407e0
Matthew Feng <mattfeng@mit.edu>
2021-11-26 17:36:15 -0500

    click to annotate

--- openpaper/pages/paper.js
+++ openpaper/pages/paper.js
@@ -9,14 +9,54 @@ import * as styles from "../styles/paper.module.scss";
 
 const _ = require("lodash");
 
+const Annotation = ({ x, y }) => {
+  const WIDTH = 50;
+  const HEIGHT = 50;
+
+  const annoStyle = {
+    position: "absolute",
+    left: x - WIDTH / 2,
+    top: y - HEIGHT / 2,
+    width: `${WIDTH}px`,
+    height: `${HEIGHT}px`,
+    border: "2px solid #f7ea36",
+    backgroundColor: "#f7ea3630",
+  };
+
+  const deleteBtn = {
+    position: "absolute",
+    left: x + WIDTH / 2 - 10,
+    top: y - HEIGHT / 2 - 10,
+    width: `20px`,
+    height: `20px`,
+    borderRadius: "50%",
+    backgroundColor: "#00000090",
+    color: "white",
+    textAlign: "center",
+    zIndex: 3,
+  };
+
+  return (
+    <>
+      <div style={deleteBtn}>
+        <span>X</span>
+      </div>
+      <div style={annoStyle}></div>
+    </>
+  );
+};
+
 class Paper extends Component {
   constructor(props) {
     super(props);
 
+    this.refs = {};
     this.state = {
       scale: 2.0,
       file: "./sample.pdf",
       numPages: null,
+      annotating: true,
+      annotations: [],
     };
   }
 
@@ -35,41 +75,73 @@ class Paper extends Component {
   onDocumentLoadSuccess = ({ numPages }) => {
     console.log(numPages);
     this.setNumPages(numPages);
+    this.refs = {};
+    for (const pg of Array(numPages).keys()) {
+      this.refs[`page${pg}`] = React.createRef();
+    }
   };
 
   round = (num, digits) => {
     return Math.round(num * Math.pow(10, digits)) / Math.pow(10, digits);
   };
 
+  handlePageClick = (e, ref) => {
+    window.getSelection().removeAllRanges();
+
+    console.log(e);
+
+    const { annotating, annotations } = this.state;
+
+    const pageX = e.nativeEvent.pageX;
+    const pageY = e.nativeEvent.pageY;
+
+    console.log(pageX, pageY);
+
+    if (annotating) {
+      const newAnnotations = annotations.concat({ x: pageX, y: pageY });
+
+      this.setState({ ...this.state, annotations: newAnnotations });
+    }
+  };
+
+  componentWillUnmount() {
+    document.removeEventListener("click", this.handlePageClick);
+  }
+
   render() {
-    const { file, numPages, scale } = this.state;
+    const { file, numPages, scale, annotations } = this.state;
+
     return (
       <div>
         <div className={styles.document}>
           <Document
-            renderMode="canvas"
+            renderMode="svg"
             file={file}
             onLoadSuccess={(e) => this.onDocumentLoadSuccess(e)}
           >
             {_.range(1, numPages + 1).map((page) => (
-              <Page
-                key={page}
-                scale={scale}
-                pageNumber={page}
-                renderTextLayer={true}
-              />
+              <>
+                <Page
+                  canvasRef={this.refs[`page${page}`]}
+                  onClick={(e) =>
+                    this.handlePageClick(e, this.refs[`page${page}`])
+                  }
+                  key={page}
+                  scale={scale}
+                  pageNumber={page}
+                  renderTextLayer={true}
+                />
+
+                <hr />
+              </>
             ))}
           </Document>
         </div>
 
-        <div className={styles.controls}>
-          <p>scale: {scale}</p>
-          <button onClick={() => this.setScale(this.round(scale - 0.1, 1))}>
-            -
-          </button>
-          <button onClick={() => this.setScale(this.round(scale + 0.1, 1))}>
-            +
-          </button>
+        <div>
+          {annotations.map((anno) => (
+            <Annotation x={anno.x} y={anno.y} />
+          ))}
         </div>
       </div>
     );
